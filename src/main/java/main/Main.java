f3ac3c32fdc9a1c7dfe4cb20cad1790e6f240f50
Ljubica <mapetovci81@gmail.com>
2021-03-21 20:47:18 +0100

    create object of class Student and set values

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,22 @@
 package main;
 
+import student.Student;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Student graduate = new Student();
+        
+        graduate.setFirstName("Petar");
+        graduate.setLastName("Petrovic");
+        graduate.setYearOfBirth(1990);
+        
+        graduate.getCourse().setName("QA");
+        graduate.getCourse().setNumberOfClasses(60);
+        
+        graduate.getComputer().setProcessTact(4.0);
+        graduate.getComputer().setMemory(16.0);
+        graduate.getComputer().setHardDrive(2000);
+        
+        graduate.printInfo();
+    }
 }
