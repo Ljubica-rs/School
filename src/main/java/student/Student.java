1f3939d80a1cea71078f73a28a2001b04dee1ad6
Ljubica <mapetovci81@gmail.com>
2021-03-21 20:45:45 +0100

    create attributes type Class, get methods, initialized objects of class Course and class Computer

--- src/main/java/student/Student.java
+++ src/main/java/student/Student.java
@@ -1,14 +1,28 @@
 package student;
 
+import course.Course;
+import computer.Computer;
+
 public class Student {
     private String firstName;
     private String lastName;
     private int yearOfBirth;
+    private Course course;
+    private Computer computer;
+    
+    public Course getCourse(){
+        return this.course;}
+     
+    public Computer getComputer(){
+        return this.computer;}
+    
     
     public Student (){
         this.firstName = "Petar";
         this.lastName = "Petrovic";
         this.yearOfBirth = 1990;
+        this.course = new Course();
+        this.computer = new Computer();
     }
     
     public Student(String firstName, String lastName, int yearOfBirth){
@@ -45,6 +59,11 @@ public class Student {
         System.out.println("First name: " + this.firstName);
         System.out.println("Last Name: " + this.lastName);
         System.out.println("Year of birth: " + this.yearOfBirth);
+        System.out.println("Course name:" + this.course.getName());
+        System.out.println("Course number of classes:" + this.course.getNumbeOfClasses());
+        System.out.println("Process tact:" + this.computer.getProcessTact());
+        System.out.println("Memory:" + this.computer.getMemory());
+        System.out.println("Hard Drive:" + this.computer.getHardDrive());
         System.out.println(" ");
     }
 }
